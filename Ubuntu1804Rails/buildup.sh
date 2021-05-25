674b2c323db80fb25153ed3866bf0ddea6227abd
Jason Brown <stopthatastronaut@gmail.com>
2021-05-26 02:22:22 +1000

    Is my mac blocked from accessing vagrant?

---
+++ Ubuntu1804Rails/buildup.sh
@@ -0,0 +1,10 @@
+﻿#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y httpd
+sudo apt-get install ruby node yarm
+
+sudo gem install rails
+
+sudo chkconfig httpd on
+sudo service httpd start
+echo "<h1>hello world</h1>" | sudo tee /var/www/html/index.html
